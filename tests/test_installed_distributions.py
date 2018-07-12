e7b4f0bebb4eedebebbda19073363804bffc5cb0
Dustin Ingram <di@users.noreply.github.com>
2018-07-12 17:57:36 -0500

    Test installed_distributions

---
+++ tests/test_installed_distributions.py
@@ -0,0 +1,20 @@
+import pip_api
+
+
+def test_installed_distributions(pip, some_distribution):
+    distributions = pip_api.installed_distributions()
+
+    assert some_distribution.name not in distributions
+
+    pip.run('install', some_distribution.filename)
+
+    distributions = pip_api.installed_distributions()
+
+    assert some_distribution.name in distributions
+
+    distribution = distributions[some_distribution.name]
+
+    assert distribution.name == some_distribution.name
+    assert distribution.version == some_distribution.version
+    assert distribution.location == some_distribution.location
+    assert distribution.editable == some_distribution.editable
