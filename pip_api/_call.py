d30318de85ea4ef3c7c1b74abe5b0d07309d9f55
Dustin Ingram <di@users.noreply.github.com>
2018-11-05 10:02:37 -0600

    Use `sys.executable` instead of `python`

--- pip_api/_call.py
+++ pip_api/_call.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
+import sys
 
 
 def call(*args):
-    python_location = os.environ.get('PIPAPI_PYTHON_LOCATION', 'python')
+    python_location = os.environ.get('PIPAPI_PYTHON_LOCATION', sys.executable)
     result = subprocess.check_output(
         [python_location, '-m', 'pip'] + list(args)
     )
