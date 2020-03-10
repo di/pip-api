93cd421290a9da96de914d85fc183b4b173f471b
Dustin Ingram <di@users.noreply.github.com>
2020-03-10 11:55:33 -0500

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="An unofficial, importable pip API",
     install_requires=["pip"],
