ddb4fc458008540a8dc78a62a4cb9ad321047240
Dustin Ingram <di@users.noreply.github.com>
2021-01-17 13:22:36 -0600

    Add support for calling from a path

--- pip_api/_call.py
+++ pip_api/_call.py
@@ -3,7 +3,9 @@ import subprocess
 import sys
 
 
-def call(*args):
+def call(*args, cwd=None):
     python_location = os.environ.get("PIPAPI_PYTHON_LOCATION", sys.executable)
-    result = subprocess.check_output([python_location, "-m", "pip"] + list(args))
+    result = subprocess.check_output(
+        [python_location, "-m", "pip"] + list(args), cwd=cwd
+    )
     return result.decode()
