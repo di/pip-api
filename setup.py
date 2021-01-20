7c3c9141a2e6eceb4bc6654a3066249af3136e03
Dustin Ingram <di@users.noreply.github.com>
2021-01-19 23:39:14 -0600

    Drop support for Python 2.7

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(
     author_email="di@python.org",
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
@@ -23,8 +22,8 @@ setup(
     long_description_content_type="text/markdown",
     name="pip-api",
     packages=find_packages(),
-    python_requires=">=2.7,!=3.0,!=3.1,!=3.2,!=3.3,!=3.4",
+    python_requires=">=3.5",
     url="http://github.com/di/pip-api",
     summary="An unofficial, importable pip API",
-    version="0.0.18",
+    version="0.1.0a0",
 )
