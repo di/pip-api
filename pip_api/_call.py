6d0ff38480fa11bf6dd983444d8acd8712bc3d71
Kyle King <KyleKing@users.noreply.github.com>
2021-03-10 21:40:06 -0500

    Fix #68 keep user's env variables

--- pip_api/_call.py
+++ pip_api/_call.py
@@ -5,7 +5,8 @@ import sys
 
 def call(*args, cwd=None):
     python_location = os.environ.get("PIPAPI_PYTHON_LOCATION", sys.executable)
+    env = {**os.environ, **{"PIP_YES": "true"}}
     result = subprocess.check_output(
-        [python_location, "-m", "pip"] + list(args), cwd=cwd, env={"PIP_YES": "true"}
+        [python_location, "-m", "pip"] + list(args), cwd=cwd, env=env
     )
     return result.decode()
