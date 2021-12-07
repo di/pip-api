e704c26de11baee48e358874004322aa0a9b74b5
William Woodruff <william@trailofbits.com>
2021-12-07 12:15:59 -0500

    pip_api/_call: pass PIP_DISABLE_PIP_VERSION_CHECK to all invocations (#114)

--- pip_api/_call.py
+++ pip_api/_call.py
@@ -5,7 +5,7 @@ import sys
 
 def call(*args, cwd=None):
     python_location = os.environ.get("PIPAPI_PYTHON_LOCATION", sys.executable)
-    env = {**os.environ, **{"PIP_YES": "true"}}
+    env = {**os.environ, **{"PIP_YES": "true", "PIP_DISABLE_PIP_VERSION_CHECK": "true"}}
     result = subprocess.check_output(
         [python_location, "-m", "pip"] + list(args), cwd=cwd, env=env
     )
