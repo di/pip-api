e46e086d508c624d26566707e6e4de067c61baad
Alex Cameron <asc@tetsuo.sh>
2022-02-01 17:27:48 +1100

    Import requirement types via pip_api module

--- pip_api/__init__.py
+++ pip_api/__init__.py
@@ -14,4 +14,8 @@ from pip_api._hash import hash
 from pip_api._installed_distributions import installed_distributions
 
 # Import these whenever, doesn't matter
-from pip_api._parse_requirements import parse_requirements
+from pip_api._parse_requirements import (
+    parse_requirements,
+    ParsedRequirement,
+    UnparsedRequirement,
+)
