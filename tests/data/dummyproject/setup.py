3ef90ae87c4a0faec3871e66705b5e674da865f5
weetster <98231586+weetster@users.noreply.github.com>
2022-07-19 12:11:49 -0400

    Editable project location (#148)
    * Added support for editable_project_location key
    
    * black formatting

---
+++ tests/data/dummyproject/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name="dummyproject",
+    version="1.0",
+    py_modules=["dummyproject"],
+)
--- tests/test_installed_distributions.py
+++ tests/test_installed_distributions.py
@@ -27,11 +27,15 @@ def test_installed_distributions(pip, some_distribution):
     assert distribution.version == some_distribution.version
 
     # Various versions of `pip` have different behavior here:
+    # * `pip` 21.3 and newer include the `editable_project_location` key in the JSON output
     # * `pip` 10.0.0b1 and newer include the `location` key in the JSON output
     # * `pip` 9.0.0 through 10.0.0b0 support JSON, but don't include `location`
     # * `pip` versions before 9.0.0 don't support JSON and don't include
     #   any location information in the textual output that we parse
-    if pip_api.PIP_VERSION >= parse("10.0.0b0"):
+    if pip_api.PIP_VERSION >= parse("21.3"):
+        assert os.path.exists(distribution.location)
+        assert not distribution.editable
+    elif pip_api.PIP_VERSION >= parse("10.0.0b0"):
         # We don't know exactly where the distribution has been installed,
         # but we know it exists and therefore is editable.
         assert os.path.exists(distribution.location)
@@ -39,6 +43,20 @@ def test_installed_distributions(pip, some_distribution):
     else:
         assert distribution.location is None
         assert not distribution.editable
+
+
+def test_installed_distributions_editable(pip, some_editable_distribution):
+    # `pip` 21.3 and newer include the `editable_project_location` key in the JSON output
+    if pip_api.PIP_VERSION >= parse("21.3"):
+        pip.run("install", "--editable", some_editable_distribution.filename)
+
+        distributions = pip_api.installed_distributions()
+        assert some_editable_distribution.name in distributions
+        distribution = distributions[some_editable_distribution.name]
+
+        assert distribution.editable_project_location is not None
+        assert os.path.exists(distribution.editable_project_location)
+        assert distribution.editable
 
 
 def test_installed_distributions_legacy_version(pip, data):
