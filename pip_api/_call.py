69fd5c482787a6e027db7264f31ae72358271d15
William Woodruff <william@trailofbits.com>
2025-01-28 14:22:59 -0500

    _call: invoke pip with PYTHONIOENCODING=utf8 (#240)
    * _call: invoke pip with PYTHONIOENCODING=utf8
    
    In principle, this should prevent Windows hosts
    from returning non-UTF-8 output in stdout/stderr
    when invoking pip.
    
    Another option here would be to set `PYTHONUTF8=1`,
    which should have a similar but more pervasive effect
    (e.g. ensuring that FS operations are all also UTF-8).
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * test latest Python/pip on Windows
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * test: test-windows must also pass
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * tox -m latest
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * conftest: fix PIPAPI_PYTHON_LOCATION on Windows
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * tests: platform-agnostic paths
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- pip_api/_call.py
+++ pip_api/_call.py
@@ -5,7 +5,13 @@ import sys
 
 def call(*args, cwd=None):
     python_location = os.environ.get("PIPAPI_PYTHON_LOCATION", sys.executable)
-    env = {**os.environ, **{"PIP_YES": "true", "PIP_DISABLE_PIP_VERSION_CHECK": "true"}}
+    env = {
+        **os.environ,
+        "PIP_YES": "true",
+        "PIP_DISABLE_PIP_VERSION_CHECK": "true",
+        # See https://github.com/di/pip-api/issues/239
+        "PYTHONIOENCODING": "utf8",
+    }
     result = subprocess.check_output(
         [python_location, "-m", "pip"] + list(args), cwd=cwd, env=env
     )
